d11360c8d2b08b2b92290873ea1e6af9346300ce
impati <impati0716@icloud.com>
2022-11-11 22:04:14 +0900

    feat : args 로그 추가

--- src/main/java/com/example/vesselcheck/domain/aspect/LogTraceAspect.java
+++ src/main/java/com/example/vesselcheck/domain/aspect/LogTraceAspect.java
@@ -17,11 +17,15 @@ public class LogTraceAspect {
         this.logTrace = logTrace;
     }
 
-    @Around("execution(* com.example.vesselcheck..*(..))")
+    @Around("execution(* com.example.vesselcheck.web..*(..)) || execution(* com.example.vesselcheck.domain.service..*(..)) || execution(* com.example.vesselcheck.domain.Repository..*(..))")
     public Object execute(ProceedingJoinPoint joinPoint) throws Throwable {
         TraceStatus status = null;
         try {
             String message = joinPoint.getSignature().toShortString();
+            for(int i = 0;i<joinPoint.getArgs().length;i++){
+                message += joinPoint.getArgs()[i] + " ";
+            }
+
             status = logTrace.begin(message);
 
             //로직 호출
