3106c86d9b2c821f80882b6555ca0cffc2b2de6e
impati <impati0716@icloud.com>
2022-11-12 16:45:18 +0900

    refactor :  FaultType 수정

--- src/main/java/com/example/vesselcheck/domain/entity/FaultType.java
+++ src/main/java/com/example/vesselcheck/domain/entity/FaultType.java
@@ -6,19 +6,20 @@ import javax.persistence.criteria.CriteriaBuilder;
 @Getter
 public enum FaultType {
     GOOD(-1,"정상"),
-    FAULT_TYPE_202(0,"단차"),
-    FAULT_TYPE_203(1,"덕트 손상"),
-    FAULT_TYPE_304(2,"오버랩"), // change
-    FAULT_TYPE_401(3,"볼트 체결 불량"),
-    FAULT_TYPE_402(4,"파이프 손상"),
-    FAULT_TYPE_204(5,"도장 불량"),
-    FAULT_TYPE_205(6,"바인딩 불량"),
-    FAULT_TYPE_206(7,"보강재 설치 불량"),
-    FAULT_TYPE_208(8,"설치 불량"),
-    FAULT_TYPE_209(9,"연결 불량"),
-    FAULT_TYPE_211(10,"케이블 손상"),
-    FAULT_TYPE_212(11,"테이프 불량"),
-    FAULT_TYPE_303(12,"언더컷"); // change
+    FAULT_TYPE_201(0,"가공 불량"),
+    FAULT_TYPE_202(1,"단차"),
+    FAULT_TYPE_203(2,"덕트 손상"), // change
+    FAULT_TYPE_205(3,"바인딩 불량"),
+    FAULT_TYPE_206(4,"보강재 설치 불량"),
+    FAULT_TYPE_207(5,"보온재 손상"),
+    FAULT_TYPE_208(6,"설치 불량"),
+    FAULT_TYPE_209(7,"연결 불량"),
+    FAULT_TYPE_210(8,"연계 처리 불량"),
+    FAULT_TYPE_211(9,"케이블 손상"),
+    FAULT_TYPE_212(10,"테이프 불량"),
+    FAULT_TYPE_213(11,"테이프 불량"),
+    FAULT_TYPE_401(12,"볼트 체결 불량"),
+    FAULT_TYPE_402(12,"파이프 손상");
 
     private Integer classId;
     private String title;
